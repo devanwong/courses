9ec0c56e40610e0b77e73cd70f4c0afd5bfafbe9
Devan Wong <devan.wong@yahoo.com>
2016-09-23 17:18:22 -0700

    first draft

--- app/controllers/Welcome.py
+++ app/controllers/Welcome.py
@@ -1,40 +1,19 @@
 """
-    Sample Controller File
+Sample Controller File
 
-    A Controller should be in charge of responding to a request.
-    Load models to interact with the database and load views to render them to the client.
+A Controller should be in charge of responding to a request.
+Load models to interact with the database and load views to render them to the client.
 
-    Create a controller using this template
+Create a controller using this template
 """
 from system.core.controller import *
-
 class Welcome(Controller):
-    def __init__(self, action):
-        super(Welcome, self).__init__(action)
-        """
-            This is an example of loading a model.
-            Every controller has access to the load_model method.
-        """
-        self.load_model('WelcomeModel')
-        self.db = self._app.db
-
-        """
-        
-        This is an example of a controller method that will load a view for the client 
-
-        """
-   
-    def index(self):
-        """
-        A loaded model is accessible through the models attribute 
-        self.models['WelcomeModel'].get_users()
-        
-        self.models['WelcomeModel'].add_message()
-        # messages = self.models['WelcomeModel'].grab_messages()
-        # user = self.models['WelcomeModel'].get_user()
-        # to pass information on to a view it's the same as it was with Flask
-        
-        # return self.load_view('index.html', messages=messages, user=user)
-        """
-        return self.load_view('index.html')
-
+	def __init__(self, action):
+		super(Welcome, self).__init__(action)
+	def index(self):
+		return self.load_view('index.html')
+	def process(self):
+		session['name'] = request.form['name']
+		return redirect('/welcome/success')
+	def success(self):
+		return self.load_view('success.html', name=session['name'])
